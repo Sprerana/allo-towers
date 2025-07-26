0227ce693793678e7310d6b1ad318b834848ffeb
Sprerana <msaiprerana@gmail.com>
2025-07-25 20:34:01 -0400

    final

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -21,10 +21,8 @@ sudo systemctl stop allo-frontend
 
 sudo systemctl start allo-frontend
 
-echo "Restarting FastAPI backend service..."
-sudo systemctl restart allo-backend.service
+sudo systemctl stop allo-frontend
 
-echo "Restarting Streamlit frontend service..."
-sudo systemctl restart allo-frontend.service
+sudo systemctl start allo-backend
 
 echo "Deployment complete."
